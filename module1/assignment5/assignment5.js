da2671d63bd2df1c40e5bd1d488ec6a41fac4d92
jpohja <pohja.johanna@gmail.com>
2025-04-03 15:08:10 +0300

    Deleted a unnecessary comment from assignment5.js

--- module1/assignment5/assignment5.js
+++ module1/assignment5/assignment5.js
@@ -1,6 +1,6 @@
 'use strict';
 const yearStr = prompt("Enter a year: ")
-//confirm("Continue")
+
 const year = parseInt(yearStr);
 if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
   document.querySelector('#year').innerHTML = 'Leap year';
